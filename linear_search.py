f3e63325e5dfce41372202cfa56ba68fcd24518d
Igor <105611667+Igor-lc@users.noreply.github.com>
2023-08-27 21:47:43 +0300

    Add files via upload

---
+++ linear_search.py
@@ -0,0 +1,12 @@
+def linear_search(values, target):
+    i = 0
+    while i < values.__len__():
+        if values[i] == target:
+            return i
+        if values[i] > target:
+            return -1
+        i += 1
+    return -1
+
+print(linear_search([4, 5, 6, 19, 4, 6], 19))
+print(linear_search([4, 5, 6, 19, 4, 6], 21))
