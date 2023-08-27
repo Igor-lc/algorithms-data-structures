11479631c33398b1b0b4557017a9ab4fbc5b5008
Igor <105611667+Igor-lc@users.noreply.github.com>
2023-08-27 21:47:25 +0300

    Add files via upload

---
+++ compound_interest.py
@@ -0,0 +1,14 @@
+def compound(amount, year_percent, months):
+    months_percent = year_percent / 12
+    for i in range(months):
+        inc = amount / 100 * months_percent
+        amount += inc
+    return amount
+
+print(compound(100000, 10, 12))
+
+
+def compound(amount, year_percent, months):
+    months_percent = year_percent / 12
+    return amount * (1 + months_percent / 100) ** months
+print(compound(100000, 10, 12))
